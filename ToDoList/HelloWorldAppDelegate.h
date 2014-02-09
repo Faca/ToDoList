6fe3d3eba6073a03c0f43086d901fc32a15665b6
Ye Xiang <1216096574@qq.com>
2014-02-09 18:26:43 +0100

    initial version

---
+++ ToDoList/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  ToDoList
+//
+//  Created by xiangye on 1/1/14.
+//  Copyright (c) 2014 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by xiangye on 1/1/14.
+//  Copyright (c) 2014 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
